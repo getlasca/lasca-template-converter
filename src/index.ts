d641c3ab609c407ee6b12716ee3749800591261c
Genki Sato <genki.sato@lasca.co.jp>
2021-02-17 08:39:33 +0900

    interface definition

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-export default function index() {
+import { Page, Condition, Loop, Event, Output } from './types'
+
+export default function convert(
+  page: Page,
+  variables: string[],
+  conditions: Condition[],
+  loops: Loop[],
+  events: Event[]
+): Output {
   console.log("test")
+  return { template: '', css: '' }
 }
---
+++ src/types.ts
@@ -0,0 +1,29 @@
+export interface Page {
+  breakpoint: Breakpoint
+  figmaObj: any
+}
+
+export interface Breakpoint {
+  min?: number
+  max?: number
+}
+
+export interface Condition {
+  nodeId: string
+  variable: string
+}
+
+export interface Loop {
+  nodeId: string
+  variable: string
+}
+
+export interface Event {
+  nodeId: string
+  name: string
+}
+
+export interface Output {
+  template: string
+  css: string
+}
