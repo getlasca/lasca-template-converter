c5a278d81d8cc789a0cf59343338597423fab5e7
kazuma ueda <ueda1023@gmail.com>
2021-03-01 08:25:21 +0900

    set FrameNode TextNode RectangleNode

--- src/builder.ts
+++ src/builder.ts
@@ -1,5 +1,7 @@
-import { Page, Embed, Condition, Loop, Event, Output } from "./types";
-import FrameNode from "./nodes/frame";
+import { Page, Embed, Condition, Loop, Event, Output } from './types'
+import FrameNode from './nodes/frame'
+import TextNode from './nodes/text'
+import RectangleNode from './nodes/rectangle'
 
 export default class Builder {
   rootNode: FrameNode;
@@ -36,14 +38,71 @@ export default class Builder {
     conditions: Condition[],
     loops: Loop[],
     events: Event[]
-  ): any {
+  ): FrameNode {
     // set rootNode from input
+
+    // FrameNode
+    const nodeId = Object.keys(page.figmaObj.nodes)[0]
+    const style = {
+      background: "a",
+      x: 1,
+      y: 1,
+      width: 1,
+      height: 1,
+      opacity: 1,
+      constraintsHorizontal: "a",
+      constraintsVertical: "a"
+    }
+    const frameNode = new FrameNode(nodeId, style, [])
+
+    // TextNode
+    page.figmaObj.nodes[nodeId].document.children.forEach((node: any) => {
+      if (node.type === "TEXT") {
+        const textStyle = {
+          background: "a",
+          color: "a",
+          fontSize: "a",
+          fontWeight: "a",
+          fontFamily: "a",
+          x: 1,
+          y: 1,
+          width: 1,
+          height: 1,
+          opacity: 1,
+          constraintsHorizontal: "a",
+          constraintsVertical: "a"
+        }
+        const textNode = new TextNode(node.id, textStyle, node.characters, [])
+        frameNode.children.push(textNode)
+      }
+    });
+
+    // RectangleNode
+    page.figmaObj.nodes[nodeId].document.children.forEach((node: any) => {
+      if (node.type === "RECTANGLE") {
+        const rectangleStyle = {
+          background: "a",
+          radius: "a",
+          x: 1,
+          y: 1,
+          width: 1,
+          height: 1,
+          opacity: 1,
+          constraintsHorizontal: "a",
+          constraintsVertical: "a"
+        }
+        const rectangleNode = new RectangleNode(node.id, rectangleStyle)
+        frameNode.children.push(rectangleNode)
+      }
+    });
+
+    return frameNode
   }
 
   private buildTemplate(): string {
     // buildTemplate
     // use children's each BaseNode.buildTemplate() method
-    return "<div>hoge</div>";
+    return this.rootNode.buildTemplate();
   }
 
   private buildCss(): string {
--- src/nodes/text.ts
+++ src/nodes/text.ts
@@ -26,7 +26,7 @@ export default class TextNode extends BaseNode {
   }
 
   buildTemplate(): string {
-    return "";
+    return "<p>" + this.text + "</p>"
   }
 
   buildCss(): string {
