4d12eb3e1c07c84f3faf3114666b9a0c2cec22f7
Genki Sato <genki.sato@lasca.co.jp>
2021-04-28 21:37:26 +0900

    add template spec

--- test/spec/template.spec.ts
+++ test/spec/template.spec.ts
@@ -3,7 +3,7 @@ import * as util from "../../src/util";
 
 import { loadFixture } from "../helper";
 
-test("convert", () => {
+test("simple", () => {
   jest.spyOn(util, "genHash").mockReturnValue("dummy");
 
   const fidmaObj = loadFixture("simple");
@@ -20,7 +20,29 @@ test("convert", () => {
   );
 });
 
-test("convert", () => {
+test("two breakpoints", () => {
+  jest.spyOn(util, "genHash").mockReturnValue("dummy");
+
+  const fidmaObj = loadFixture("simple");
+  const output = convert(
+    {
+      breakpoints: [
+        { figmaObj: fidmaObj, range: { max: 349 } },
+        { figmaObj: fidmaObj, range: { min: 350 } },
+      ],
+    },
+    [],
+    [],
+    [],
+    [],
+    []
+  );
+  expect(output.template).toBe(
+    `<div><div class="breakpoint-dummy"><div class="class-dummy"><div></div><p class="class-dummy">sampleText</p></div></div><div class="breakpoint-dummy"><div class="class-dummy"><div></div><p class="class-dummy">sampleText</p></div></div></div>`
+  );
+});
+
+test("nested", () => {
   jest.spyOn(util, "genHash").mockReturnValue("dummy");
 
   const fidmaObj = loadFixture("nested");
