f3fd4d1b3ad038fbbef4e6c803b336378a9f0f1f
Genki Sato <genki.sato@lasca.co.jp>
2021-07-07 19:28:33 +0900

    fix test

--- test/spec/css.spec.ts
+++ test/spec/css.spec.ts
@@ -4,7 +4,15 @@ import { loadFixture } from "../helper";
 test("simple", () => {
   const figma = loadFixture("simple");
   const output = convert([
-    { figma: figma, variables: [], conditions: [], loops: [], events: [] },
+    {
+      figma: figma,
+      mixedTexts: [],
+      nodeImages: [],
+      variables: [],
+      conditions: [],
+      loops: [],
+      events: [],
+    },
   ]);
   expect(output.css).toBe(
     ".class-1 { background-color: rgba(255,255,255,1); position: relative; height: 845px; }" +
@@ -19,6 +27,8 @@ test("two breakpoints", () => {
     {
       figma: figma,
       max: 349,
+      mixedTexts: [],
+      nodeImages: [],
       variables: [],
       conditions: [],
       loops: [],
@@ -27,6 +37,8 @@ test("two breakpoints", () => {
     {
       figma: figma,
       min: 350,
+      mixedTexts: [],
+      nodeImages: [],
       variables: [],
       conditions: [],
       loops: [],
@@ -50,7 +62,15 @@ test("two breakpoints", () => {
 test("nested", () => {
   const figma = loadFixture("nested");
   const output = convert([
-    { figma: figma, variables: [], conditions: [], loops: [], events: [] },
+    {
+      figma: figma,
+      mixedTexts: [],
+      nodeImages: [],
+      variables: [],
+      conditions: [],
+      loops: [],
+      events: [],
+    },
   ]);
   expect(output.css).toBe(
     ".class-1 { background-color: rgba(255,255,255,1); position: relative; height: 800px; }" +
