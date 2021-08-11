cbb669cf59c819863ca2fe27a38a53dee3ade874
Genki Sato <genki.sato@lasca.co.jp>
2021-08-11 12:23:08 +0900

    remove logs

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,5 @@ import { Breakpoint, Output } from "./types";
 import Builder from "./builder";
 
 export default function convert(breakpoints: Breakpoint[]): Output {
-  const output = new Builder(breakpoints).build();
-  console.log("converter template: " + output.template);
-  console.log("converter css: " + output.css);
-  return output;
+  return new Builder(breakpoints).build();
 }
