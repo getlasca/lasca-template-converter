87825da04125ec9d94463978c17b81d014252888
Genki Sato <genki.sato@lasca.co.jp>
2021-11-04 15:01:36 +0900

    add fill container spec

--- test/spec/css/autolayout/vertical.spec.ts
+++ test/spec/css/autolayout/vertical.spec.ts
@@ -199,3 +199,71 @@ test("horizontal left space between", () => {
       ".class-2 { background-color: rgba(255,255,255,1); width: 500px; height: 500px; flex: 0 0 500px; position: relative; }"
   );
 });
+
+test("vertical fill container vertically", () => {
+  const figmaFixture = buildFigmaFixture({
+    type: "FRAME",
+    id: "10:0",
+    layoutMode: "VERTICAL",
+    counterAxisAlignItems: "MIN",
+    primaryAxisAlignItems: "MIN",
+    itemSpacing: 20,
+    paddingTop: 40,
+    paddingLeft: 50,
+    paddingBottom: 40,
+    paddingRight: 50,
+    children: [
+      {
+        type: "RECTANGLE",
+        id: "10:1",
+        layoutGrow: 1,
+      },
+    ],
+  });
+  const output = convert([
+    {
+      ...defaultConvertParams,
+      figma: figmaFixture,
+    },
+  ]);
+  expect(output.css).toBe(
+    ".breakpoint-1 { position: relative; } " +
+      ".class-1 { background-color: rgba(255,255,255,1); box-sizing: border-box; display: flex; flex-direction: column; padding-top: 40px; padding-bottom: 40px; padding-left: 50px; padding-right: 50px; justify-content: flex-start; align-items: flex-start; }" +
+      " .class-1 > *:not(:last-child) { margin-bottom: 20px; }" +
+      ".class-2 { background-color: rgba(255,255,255,1); width: 500px; height: 500px; flex: 1; position: relative; }"
+  );
+});
+
+test("vertical fill container horizontally", () => {
+  const figmaFixture = buildFigmaFixture({
+    type: "FRAME",
+    id: "10:0",
+    layoutMode: "VERTICAL",
+    counterAxisAlignItems: "MIN",
+    primaryAxisAlignItems: "MIN",
+    itemSpacing: 20,
+    paddingTop: 40,
+    paddingLeft: 50,
+    paddingBottom: 40,
+    paddingRight: 50,
+    children: [
+      {
+        type: "RECTANGLE",
+        id: "10:1",
+        layoutAlign: "STRETCH",
+      },
+    ],
+  });
+  const output = convert([
+    {
+      ...defaultConvertParams,
+      figma: figmaFixture,
+    },
+  ]);
+  expect(output.css).toBe(
+    ".breakpoint-1 { position: relative; } " +
+      ".class-1 { background-color: rgba(255,255,255,1); box-sizing: border-box; display: flex; flex-direction: column; padding-top: 40px; padding-bottom: 40px; padding-left: 50px; padding-right: 50px; justify-content: flex-start; align-items: flex-start; }" +
+      " .class-1 > *:not(:last-child) { margin-bottom: 20px; }" +
+      ".class-2 { background-color: rgba(255,255,255,1); width: 100%; height: 500px; flex: 0 0 500px; position: relative; }"
+  );
+});
